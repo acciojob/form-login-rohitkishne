83735d3e0b446b2c0708ae9f34dc1d50312e06a7
Rohit kishne <92744319+rohitkishne@users.noreply.github.com>
2023-07-06 00:29:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+	fname = fname[0].toUpperCase() + fname.slice(1);
+    lname = lname[0].toUpperCase() + lname.slice(1);
+    let fullName = fname +" "+lname
+    alert(fullName)
 
 }
